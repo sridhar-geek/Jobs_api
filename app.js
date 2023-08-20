abcf083799ad28cae03c98f326460d254ec7e096
Sridhar <sridharmoturu@gmail.com>
2023-08-20 08:54:30 +0530

    final commit

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ const app = express();
       // IMPORTS
 require('dotenv').config();
 require('express-async-errors');
+
+// security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 //MongoDb connect 
 const connectDB = require("./db/connect")
+
 // routes
 const userRoutes = require('./routes/auth')
 const jobsRoutes = require('./routes/jobs')
 const authenication = require('./middleware/authentication')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());    // without this function we can't be albe to access req.body elements 
+app.use( helmet() )
+app.use( cors() )
+app.use( xss() )
+app.use(  rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	// standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	// legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	// store: ... , // Use an external store for more precise rate limiting
+}) )
 
 
       // Middleware Routes
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,31 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if(err.name === 'ValidationError'){
+      customError.msg = Object.values(err.errors)
+      .map( (item) => item.message).join(',')
+      customError.statusCode = 400
+  }
+
+  if(err.code && err.code === 11000){
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} feild, please enter another value`
+    customError.statusCode = 400
+  }
+
+  if(err.name === 'CastError'){
+    CustomAPIError.msg = `No item found with id : ${err.value}`
+    customError.statusCode = 404
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
